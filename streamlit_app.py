0659bb903046c2753128e35c9c27f3ea190af1a1
Sai Venkatesh Namburi <namburi.saivenkatesh@gmail.com>
2023-02-09 10:27:30 +0000

    Update streamlit_app.py
    modified text lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Bluberry Oatmeal')
-streamlit.text('Kale, Spinach')
-streamlit.text('egg')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
