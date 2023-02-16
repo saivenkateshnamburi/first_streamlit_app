3faf3847419bd7d8753a11d8753a9268a7acc4c3
Sai Venkatesh Namburi <namburi.saivenkatesh@gmail.com>
2023-02-16 01:55:00 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,3 +67,12 @@ if streamlit.button('Add a Fruit to the List'):
       my_cnx = snowflake.connector.connect (**streamlit.secrets["snowflake"])
       back_from_function = insert_row_snowflake(add_my_fruit)
       streamlit.text(back_from_function)
+  
+
+
+
+if streamlit.button('Get Fruit list'):
+      my_cnx = snowflake.connector.connect (**streamlit.secrets["snowflake"])
+      my_data_rows = get_fruit_load_list()
+      my_cnx.close()
+      streamlit.dataframe(my_data_rows)
